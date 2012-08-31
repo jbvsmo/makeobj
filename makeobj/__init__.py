a3de0bbf742b2d09fabebbd8b8bad1af8a9c00f8
Bernardo Vianna <jbvsmo@gmail.com>
2012-08-30 23:29:06 -0300

    Added `make_obj` and `Obj` to the top of the package with `parse`

--- makeobj/__init__.py
+++ makeobj/__init__.py
@@ -6,14 +6,17 @@
 """
 
 from makeobj.text_parse import _parse, _iter_parse
+from makeobj.obj import make_object, Obj
 
 __author__ = 'JB'
 __metaclass__ = type
+__all__ = ('parse', 'make_object', 'Obj')
 
 
 def parse(text, upto=None):
     """ Parse a block of text in makeobj format and create a list of elements
         Works with file handlers, multiline strings and other iterables
+        The second argument can make the text parsing stop in a certain line if given.
     """
     if not hasattr(text, 'readlines'):
         try:
@@ -26,3 +29,4 @@ def parse(text, upto=None):
 
 if __name__ == '__main__':
     print(parse(open('../tests/f1.makeobj')))
+    #print(parse(open(r'E:\Dropbox\dev\poke\mon\info\stats.makeobj')))
