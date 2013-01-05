855e58918ae4185e08120966c7219bac91078289
Bernardo Vianna <jbvsmo@gmail.com>
2013-01-04 23:57:21 -0200

    Better description and keywords

--- setup.py
+++ setup.py
@@ -7,6 +7,40 @@ MakeObj is a module to help create powerful enumeration classes with support
 to attributes specific for each element.
 
 This module is compatible with Python versions 2.6+ and 3.0+
+
+Usage:
+
+>>> from makeobj import Obj, keys, attr
+>>> class RGBColors(Obj):
+...     red, green, blue = keys(3)
+...     hex = attr('ff0000', '00ff00', '0000ff')
+...
+>>> RGBColors
+<Object: RGBColors -> [red:0, green:1, blue:2]>
+>>> RGBColors.red
+<Value: RGBColors.red = 0>
+>>> RGBColors.blue.hex
+'0000ff'
+
+
+Using the custom Enum Language:
+
+
+::
+
+  @obj RGBColors =:
+      @keys = 'red', 'green', 'blue'
+      @attr hex = 'ff0000', '00ff00', '0000ff'
+
+
+Then you can parse it:
+
+>>> from makeobj import parse
+>>> RGBColors = parse(text)
+>>> RGBColors.red
+<Value: RGBColors.red = 0>
+
+.
 '''
 
 setup(
@@ -18,7 +52,8 @@ setup(
     author = 'João Bernardo Oliveira',
     author_email = 'jbvsmo@example.com',
     description = 'Powerful Enumeration System',
-    long_description=desc,
+    long_description = desc,
+    keywords = ['enum', 'enumeration', 'Enum Language'],
     classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
